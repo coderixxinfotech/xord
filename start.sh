aca404d39f126ea53aa202ff228cd4cb2137d394
crypticmeta <crypticmeta@coderixx.com>
2024-02-28 12:17:57 +0530

    new bitcoin ip

--- start.sh
+++ start.sh
@@ -31,7 +31,7 @@ fi
 retries=0
 while [ $retries -lt 3 ]; do
     # Start the application
-    ord --rpc-url bitcoin-container:8332 --bitcoin-rpc-user mempool --bitcoin-rpc-pass mempool --data-dir /root/.local/share/ord/$runDir  --index-only-first-transfer --index-transfer-history --filter-metaprotocol cbrc-20 --filter-metaprotocol "@" server --http-port 8080 -j &>/dev/stdout &
+    ord --rpc-url 146.190.11.6:8332 --bitcoin-rpc-user mempool --bitcoin-rpc-pass mempool --data-dir /root/.local/share/ord/$runDir  --index-only-first-transfer --index-transfer-history --filter-metaprotocol cbrc-20 --filter-metaprotocol "@" server --http-port 8080 -j &>/dev/stdout &
     
     # Sleep for a few seconds to allow the server to start up
     sleep 5
@@ -69,9 +69,9 @@ while true; do
     if ! pgrep -x "pv" > /dev/null
     then
         echo "Checking wallet balance using main index.redb..."
-        ord --rpc-url bitcoin-container:8332 --bitcoin-rpc-user mempool --bitcoin-rpc-pass mempool --wallet ord index update
+        ord --rpc-url 146.190.11.6:8332 --bitcoin-rpc-user mempool --bitcoin-rpc-pass mempool --wallet ord index update
         echo "Checking wallet balance using index.redb not being used as server..."
-        ord --rpc-url bitcoin-container:8332 --bitcoin-rpc-user mempool --bitcoin-rpc-pass mempool --data-dir /root/.local/share/ord/$copyDir --wallet ord index update
+        ord --rpc-url 146.190.11.6:8332 --bitcoin-rpc-user mempool --bitcoin-rpc-pass mempool --data-dir /root/.local/share/ord/$copyDir --wallet ord index update
     fi
     sleep 1800
 done
